0dc5e3ec04157861e8fd740b677c07c5dc95ea7a
Ridhosflhq <104475526+Ridhosflhq@users.noreply.github.com>
2025-03-16 16:29:34 +0700

    Update script.js

--- Peta_6/script.js
+++ Peta_6/script.js
@@ -1,12 +1,12 @@
 // Inisialisasi peta
-const map = L.map('map').setView([-7.479734, 110.217711], 13); // Koordinat Kota Magelang
+const map = L.map('map').setView([-7.486121, 110.222775], 15); // Fokus ke titik pertama
 
 // Tambahkan layer peta dasar dari OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// URL file GeoJSON dari Google Drive (ubah ID sesuai dengan file kamu)
+// URL file GeoJSON dari Google Drive (gunakan ID dari link)
 const geojsonUrl = 'https://drive.google.com/uc?id=1KGgEiJmVXl71rPfd7Dqh8hjDwSaEd2Hr';
 
 // Ambil data GeoJSON dari Google Drive dan tambahkan ke peta
@@ -17,15 +17,15 @@ fetch(geojsonUrl)
     L.geoJSON(data, {
       pointToLayer: (feature, latlng) => {
         return L.circleMarker(latlng, {
-          radius: 4, // Ukuran dot
+          radius: 5, // Ukuran dot
           fillColor: '#ff5733', // Warna dot
           color: '#ffffff', // Warna border dot
           weight: 1,
           opacity: 1,
           fillOpacity: 0.8
         }).bindPopup(`
-          <b>Nama Kelurahan:</b> ${feature.properties.kelurahan}<br>
-          <b>Jumlah Penduduk:</b> ${feature.properties.populasi}
+          <b>Kelurahan:</b> ${feature.properties.kelurahan || 'Tidak Ada'}<br>
+          <b>Jumlah Penduduk:</b> ${feature.properties.Jumlah || 'Tidak Ada'}
         `);
       }
     }).addTo(map);
